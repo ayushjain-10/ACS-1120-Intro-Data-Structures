19e2dc180583b3707c10900d79e01c3b22134f6c
Ayush Jain <ayushshreeshreemal@gmail.com>
2023-03-03 23:13:30 +0530

    Analyze Word Frequency in Text

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -7,14 +7,10 @@ def read_words_file(file_path):
         words = [word.strip() for word in words]
         return words
 
-def generate_sentence(words, num_words):
-    sentence_words = random.sample(words, num_words)
-    sentence = ' '.join(sentence_words)
-    return sentence
-
 if __name__ == '__main__':
     file_path = '/usr/share/dict/words'  # replace with path to your file of words
-    num_words = int(sys.argv[1])  # replace with number of words you want in the sentence
+    num_words = int(sys.argv[1]) if len(sys.argv) > 1 else 5
     words = read_words_file(file_path)
-    sentence = generate_sentence(words, num_words)
+    sentence_words = random.sample(words, num_words)
+    sentence = ' '.join(sentence_words)
     print(sentence)
